17d8334e5afd383da9d69604a39a2f5ae00bc024
mick84 <demenkoff1984@yahoo.com>
2022-09-27 20:05:57 +0300

    form style ready

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,18 @@
 const buttonMenu = document.getElementById("btn-menu");
 const mobileMenu = document.getElementById("mobile-menu");
+const form = document.getElementById("form");
 buttonMenu.onclick = function () {
   // console.log(this);
   this.classList.toggle("active");
   mobileMenu.classList.toggle("active");
+};
+mobileMenu.onclick = function () {
+  setTimeout(() => {
+    this.classList.toggle("active");
+    buttonMenu.classList.toggle("active");
+  }, 500);
+};
+
+form.onsubmit = function (e) {
+  e.preventDefault();
 };
